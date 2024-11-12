b005014c3e911170097efa4d33c217a8d0b674f7
Pub3rty <anietiewisdom45y@gmail.com>
2024-11-12 10:17:38 +0100

    Update start_qrl.sh

--- start_qrl.sh
+++ start_qrl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 reset
 
-./SRBMiner-MULTI --algorithm randomx --pool de.qrl.herominers.com:1166 --wallet Q0105005e471114865647d265142795f2eac49fc2796e8af3f901214feeef6ceb8f4dcdb5a17470 --password YOUR_WORKER_NAME --cpu-threads 0 --disable-gpu
+./SRBMiner-MULTI --algorithm randomx --pool de.qrl.herominers.com:1166 --wallet Q0105005e471114865647d265142795f2eac49fc2796e8af3f901214feeef6ceb8f4dcdb5a17470 --password YOUR_WORKER_NAME054 --cpu-threads 8 --disable-gpu
