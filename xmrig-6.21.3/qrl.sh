3b691267ceeedab712d2e492e48f79bf92c1aff6
Pub3rty <anietiewisdom45y@gmail.com>
2024-11-12 10:56:03 +0100

    Add files via upload

---
+++ xmrig-6.21.3/qrl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./xmrig --donate-level 1 -o de.qrl.herominers.com:1166 -u Q0105005e471114865647d265142795f2eac49fc2796e8af3f901214feeef6ceb8f4dcdb5a17470 -p YOUR_WORKER_NAME -a rx/0 -k 
